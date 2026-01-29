7866da185b6a3d3884fbeb7ecadcaa7149073ddd
username_github <email_github>
2026-01-29 08:28:34 +0700

    alby ganteng

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -219,7 +219,7 @@
 						<div class="relative z-10 w-5 h-5">
 							<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
-    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+	d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
 </svg>
 						</div>
 						<span class="font-semibold text-sm relative z-10">Admin</span>
